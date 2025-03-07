6984381b3e294854e6d7fb0c407c1900f11b58bd
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2025-03-07 11:27:35 +0000

    feat: add initial codebase

---
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/person/domain/Person.java
@@ -0,0 +1,51 @@
+package pt.ulisboa.tecnico.rnl.dei.dms.person.domain;
+
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+import pt.ulisboa.tecnico.rnl.dei.dms.person.dto.PersonDto;
+
+@Data
+@Entity
+@Table(name = "people")
+public class Person {
+
+	public enum PersonType {
+		COORDINATOR, STAFF, STUDENT, TEACHER, SC
+	}
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "ist_id", nullable = false, unique = true)
+	private String istId;
+
+	@Column(name = "type", nullable = false)
+	@Enumerated(EnumType.STRING)
+    private PersonType type;
+
+
+	// TODO: maybe add more fields? ...or maybe not? what makes sense here?
+
+	protected Person() {
+	}
+
+	public Person(String name, String istId, PersonType type) {
+		this.name = name;
+		this.istId = istId;
+		this.type = type;
+	}
+
+	public Person(PersonDto personDto) {
+		this(personDto.name(), personDto.istId(),
+				PersonType.valueOf(personDto.type().toUpperCase()));
+		System.out.println("PersonDto: " + personDto);
+		System.out.println("PersonType: " + personDto.type());
+
+	}
+}
