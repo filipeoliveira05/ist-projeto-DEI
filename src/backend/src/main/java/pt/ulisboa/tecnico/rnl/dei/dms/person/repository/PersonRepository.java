51ba933ea90713c13d9072e3966ed4a9f1d1772c
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2025-03-09 11:09:23 +0000

    feat: added new methods to find by specific fields

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/person/repository/PersonRepository.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/person/repository/PersonRepository.java
@@ -1,7 +1,11 @@
 package pt.ulisboa.tecnico.rnl.dei.dms.person.repository;
 
+import java.util.Optional;
+import java.util.List;
+//import java.lang.foreign.Linker.Option;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.util.Optionals;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +14,10 @@ import pt.ulisboa.tecnico.rnl.dei.dms.person.domain.Person;
 @Repository
 @Transactional
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
+    Optional<Person> findByName(String name);
+    Optional<Person> findByIstId(String istId);
+    List<Person> findByType(Person.PersonType type);
+    Optional<Person> findByEmail(String email);
+    List<Person> findByStatus(boolean status);
+    Optional<Person> findByPhoneNumber(String phoneNumber);
 }
