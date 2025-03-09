e07f01929c1c5fdb10dedc36f260a9ec3681cb61
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2025-03-09 23:07:22 +0000

    refactor: status attribute is true by default

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/person/dto/PersonDto.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/person/dto/PersonDto.java
@@ -3,9 +3,15 @@ package pt.ulisboa.tecnico.rnl.dei.dms.person.dto;
 import pt.ulisboa.tecnico.rnl.dei.dms.person.domain.Person;
 
 // Data Transfer Object, to communicate with frontend
-public record PersonDto(long id, String name, String istId, String type, String email, boolean status, String phoneNumber) {
+public record PersonDto(Long id, String name, String istId, String type, String email, Boolean status, String phoneNumber) {
 	public PersonDto(Person person) {
 		this(person.getId(), person.getName(), person.getIstId(),
-				person.getType().toString(), person.getEmail(), person.isStatus(), person.getPhoneNumber());
+			 person.getType().toString(), person.getEmail(),
+			 person.isStatus(), person.getPhoneNumber());
 	}
+
+	public Boolean status() {
+        return status != null ? status : true;
+    }
+
 }
