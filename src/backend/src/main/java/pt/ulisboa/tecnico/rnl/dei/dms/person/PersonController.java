c65389da1e21c6ebeebd0b36db895ba35727e309
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2025-03-09 11:12:03 +0000

    feat: changed createPerson; added getPeopleByType, deactivatePerson

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/person/PersonController.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/person/PersonController.java
@@ -3,14 +3,18 @@ package pt.ulisboa.tecnico.rnl.dei.dms.person;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import pt.ulisboa.tecnico.rnl.dei.dms.person.domain.Person;
 import pt.ulisboa.tecnico.rnl.dei.dms.person.dto.PersonDto;
 import pt.ulisboa.tecnico.rnl.dei.dms.person.service.PersonService;
 
@@ -25,8 +29,9 @@ public class PersonController {
 	}
 
 	@PostMapping("/people")
-	public PersonDto createPerson(@RequestBody PersonDto personDto) {
-		return personService.createPerson(personDto);
+	public ResponseEntity<PersonDto> createPerson(@RequestBody PersonDto personDto) {
+		PersonDto createdPerson = personService.createPerson(personDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdPerson);
 	}
 
 	@GetMapping("/people/{id}")
@@ -34,9 +39,20 @@ public class PersonController {
 		return personService.getPerson(id);
 	}
 
+	@GetMapping("/people/type/{type}")
+	public List<PersonDto> getPeopleByType(@PathVariable String type) {
+		return personService.getPeopleByType(type);
+	}
+
 	@PutMapping("/people/{id}")
 	public PersonDto updatePerson(@PathVariable long id, @RequestBody PersonDto personDto) {
 		return personService.updatePerson(id, personDto);
+	}
+
+	@PatchMapping("/people/{id}/deactivate")
+	public ResponseEntity<Void> deactivatePerson(@PathVariable long id) {
+		personService.deactivatePerson(id);
+		return ResponseEntity.noContent().build();
 	}
 
 	@DeleteMapping("/people/{id}")
