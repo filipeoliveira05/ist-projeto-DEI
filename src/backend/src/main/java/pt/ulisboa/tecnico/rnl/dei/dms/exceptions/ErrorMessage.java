9769cb79ad1abd728be0273688da7a5866483a4b
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2025-03-09 11:06:01 +0000

    feat: added new error messages

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/exceptions/ErrorMessage.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/exceptions/ErrorMessage.java
@@ -3,8 +3,14 @@ package pt.ulisboa.tecnico.rnl.dei.dms.exceptions;
 public enum ErrorMessage {
 
 	NO_SUCH_PERSON("Não existe nenhuma pessoa com o ID %s", 1001),
-	PERSON_NAME_NOT_VALID("O nome da pessoa especificado não é válido.", 1002),
-	PERSON_ALREADY_EXISTS("Já existe uma pessoa com o ID %s", 1003);
+	PERSON_ALREADY_EXISTS("Já existe uma pessoa com o ID %s", 1002),
+	PERSON_NAME_NOT_VALID("O nome da pessoa especificado não é válido.", 1003),
+	IST_ID_NOT_VALID("O IST ID especificado não é válido.", 1004),
+	DUPLICATE_IST_ID("Já existe uma pessoa com o IST ID %s", 1005),
+	EMAIL_NOT_VALID("O email especificado não é válido.", 1006),
+	DUPLICATE_EMAIL("Já existe uma pessoa com o email %s", 1007),
+	PHONE_NUMBER_NOT_VALID("O número de telefone especificado não é válido.", 1008),
+	DUPLICATE_PHONE_NUMBER("Já existe uma pessoa com o número de telefone %s", 1009);
 
 	private final String label;
 	private final int code;
